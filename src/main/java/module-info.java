eb53a1bba73992c5f75e6068a96110bb9b5a7baf
minzero31 <77minyoung77@gmail.com>
2024-10-27 22:37:31 +0900

    Build : build.gradle 스프링 프로젝트로 변경

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.thenightof.aiit.thenightofaiitlogin {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.thenightof.aiit.thenightofaiitlogin to javafx.fxml;
-    exports com.thenightof.aiit.thenightofaiitlogin;
-}
